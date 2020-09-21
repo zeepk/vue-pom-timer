d3f2d6ba9e8c2fd27f864e4215928453b1318408
zeepk <zeepkdb@gmail.com>
2020-09-21 00:17:45 -0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Dashboard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from "./components/Dashboard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Dashboard,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>Timer</h1>
+    <h1>{{seconds}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: () => ({
+    seconds: 500,
+  }),
+  mounted() {
+    console.log("mounted");
+    this.updateTimer();
+  },
+  methods: {
+    updateTimer() {
+      const timer = setInterval(() => {
+        if (this._seconds === 100) {
+          clearInterval(timer);
+          return;
+        }
+        // console.log(this.seconds);
+        this.seconds--;
+      }, 1000);
+    },
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
